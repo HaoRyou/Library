75f27ede6643bee2f83976c46541820d4f262a86
HaoRyou <158393369+HaoRyou@users.noreply.github.com>
2025-07-15 20:01:26 -0400

    modified: Able to connect js with html Create button that will submit all user input to the js where js will reflesh the child everytime something is added or deleted.
    modified: style.css and main.js for display
    allow pop-up when user want to add more books, clear input box after submitting.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+const addbooks = document.getElementById("Books");
+
 const mylibrary = [];
 
 function Book(title, author, pages, read){
-    if(Lnew.target){
+    if(!new.target){
         throw Error("You must use 'new operator to call the constructor");
     }
     this.id = crypto.randomUUID();
@@ -9,20 +11,74 @@ function Book(title, author, pages, read){
     this.author=author;
     this.pages=pages;
     this.read=read;
-    info = console.log(`${title} by ${author}, ${pages} pages,  ${read ? "has read" :"not read yet" }`)
+    this.info = `${title} by ${author}, ${pages} pages,  ${read ? "has read" :"not read yet" }`;
 }
 
 function addBookToLibrary(title,author,page,read){
-    books = new Book(title,author,page,read);
+    let books = new Book(title,author,page,read);
 
     mylibrary.push(books);
 }
 
-mylibrary.forEach(books => { books.info;
-    
-});
+//Testing Books
+addBookToLibrary("Final Game","Steven",150,false);
+addBookToLibrary("End Game","Jobs",150,true);
+addBookToLibrary("Fighting for life","Kim",150,true);
+addBookToLibrary("The seven Sin","LI",150,false);
+bookstohtml();
+//
 
-addBookToLibrary(Final,Steve,150,false);
-addBookToLibrary(end,job,150,true);
-addBookToLibrary(fight,kim,150,true);
-addBookToLibrary(sin,li,150,false);
+function bookstohtml(){
+    while(addbooks.firstChild){
+        addbooks.removeChild(addbooks.firstChild);
+    }
+
+    mylibrary.forEach(books => {
+        const div = document.createElement('div');
+        div.className='book';
+        div.textContent = books.info;
+
+        const removebook = document.createElement('button');
+        removebook.textContent = "Delete";
+        removebook.onclick = () =>{
+            deletebook(books.id);
+        }
+        div.appendChild(removebook);
+        addbooks.appendChild(div);
+    });
+}
+
+function deletebook(id){
+    const index = mylibrary.findIndex(book =>book.id == id);
+    if(index !==-1){
+        mylibrary.splice(index,1);//delete book
+        bookstohtml(); //refresh the page
+    }
+}
+
+
+const popup = document.getElementById('popups');
+
+function openpopup(){
+    popup.classList.add("open-popup");
+}
+
+function closepopup(){
+    popup.classList.remove('open-popup')
+}
+
+function submit_work(){
+    const usertitle = document.getElementById('Title').value;
+    const userauthor = document.getElementById('Author').value;
+    const userpage = document.getElementById('Page').value;
+    const userhasread = document.getElementById('HasRead').checked;
+    addBookToLibrary(usertitle,userauthor,userpage,userhasread);
+    bookstohtml();
+    
+    document.getElementById('Title').value ='';
+    document.getElementById('Author').value='';
+    document.getElementById('Page').value='';
+    document.getElementById('HasRead').checked='';
+    closepopup();
+}
+
