9e65e5f28cff49f341dd216890af32e464ad1a5f
HaoRyou <158393369+HaoRyou@users.noreply.github.com>
2025-07-17 13:06:00 -0400

    modified: Use class to replace function Replacing function with class Books and use static to store the library. Add another button that will change read status.

--- main.js
+++ main.js
@@ -1,30 +1,56 @@
 const addbooks = document.getElementById("Books");
 
-const mylibrary = [];
+// const mylibrary = [];
 
-function Book(title, author, pages, read){
-    if(!new.target){
+
+class Book{
+    static mylibrary = [];
+
+    constructor(title,author,pages,read){
+        if(!new.target){
         throw Error("You must use 'new operator to call the constructor");
+        }
+        this.title = title;
+        this.author = author;
+        this.pages=pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
+        this.info = `${title} by ${author}, ${pages} pages,  ${read ? "has read" :"not read yet" }`;
     }
-    this.id = crypto.randomUUID();
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
-    this.info = `${title} by ${author}, ${pages} pages,  ${read ? "has read" :"not read yet" }`;
+
+    static addBookToLibrary(books){
+        Book.mylibrary.push(books);
+    }
+
 }
 
-function addBookToLibrary(title,author,page,read){
-    let books = new Book(title,author,page,read);
 
-    mylibrary.push(books);
-}
+
+// function Book(title, author, pages, read){
+//     if(!new.target){
+//         throw Error("You must use 'new operator to call the constructor");
+//     }
+//     this.id = crypto.randomUUID();
+//     this.title=title;
+//     this.author=author;
+//     this.pages=pages;
+//     this.read=read;
+//     this.info = `${title} by ${author}, ${pages} pages,  ${read ? "has read" :"not read yet" }`;
+// }
+
+// function addBookToLibrary(title,author,page,read){
+//     let books = new Book(title,author,page,read);
+
+//     mylibrary.push(books);
+// }
 
 //Testing Books
-addBookToLibrary("Final Game","Steven",150,false);
-addBookToLibrary("End Game","Jobs",150,true);
-addBookToLibrary("Fighting for life","Kim",150,true);
-addBookToLibrary("The seven Sin","LI",150,false);
+
+Book.addBookToLibrary(new Book("Final Game","Steven",150,false));
+Book.addBookToLibrary(new Book ("End Game","Jobs",150,true));
+Book.addBookToLibrary(new Book("Fighting for life","Kim",150,true));
+Book.addBookToLibrary(new Book("The seven Sin","LI",150,false));
+
 bookstohtml();
 //
 
@@ -33,25 +59,46 @@ function bookstohtml(){
         addbooks.removeChild(addbooks.firstChild);
     }
 
-    mylibrary.forEach(books => {
+    Book.mylibrary.forEach(books => {
         const div = document.createElement('div');
         div.className='book';
         div.textContent = books.info;
+
+        const hasread = document.createElement('button');
+        let reading = `${books.read ? "has read" :"not read yet" }`;
+
+        hasread.textContent = reading;
+        hasread.onclick = () =>{
+            if(hasread.textContent == "has read"){
+                hasread.textContent = "not read yet";
+            }
+            else{
+                hasread.textContent = "has read";
+            }
+        }
+
+        const div2 = document.createElement('div');
+        div2.className='button_div';
+        
+        
+        div2.appendChild(hasread);
 
         const removebook = document.createElement('button');
         removebook.textContent = "Delete";
         removebook.onclick = () =>{
             deletebook(books.id);
         }
-        div.appendChild(removebook);
+        div2.appendChild(removebook);
+        div.appendChild(div2);
+        
         addbooks.appendChild(div);
     });
 }
 
 function deletebook(id){
-    const index = mylibrary.findIndex(book =>book.id == id);
+    const index = Book.mylibrary.findIndex(book =>book.id == id);
     if(index !==-1){
-        mylibrary.splice(index,1);//delete book
+        Book.mylibrary.splice(index,1);//delete book
         bookstohtml(); //refresh the page
     }
 }
@@ -72,7 +119,7 @@ function submit_work(){
     const userauthor = document.getElementById('Author').value;
     const userpage = document.getElementById('Page').value;
     const userhasread = document.getElementById('HasRead').checked;
-    addBookToLibrary(usertitle,userauthor,userpage,userhasread);
+    Book.addBookToLibrary(new Book(usertitle,userauthor,userpage,userhasread));
     bookstohtml();
     
     document.getElementById('Title').value ='';
